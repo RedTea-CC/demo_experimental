fbe578ab7197ee6504be0730bb662ab4329015eb
liuyilong <redtea25@qq.com>
2024-04-15 17:49:50 +0800

    深克隆：解决对象的循环引用

--- Function-tool/clone.js
+++ Function-tool/clone.js
@@ -18,11 +18,37 @@ function deepClone(target) {
   if (typeof target !== "object" || target === null) {
     return target;
   }
-
-  let clone = {};
+  let clone = Array.isArray(target) ? [] : {};
   for (let key in target) {
-    // 递归克隆
-    clone[key] = deepClone(target[key]);
+    if (Object.prototype.hasOwnProperty.call(target, key)) {
+      // 递归克隆
+      clone[key] = deepClone(target[key]);
+    }
+  }
+  return clone;
+}
+
+// 深克隆函数，解决循环引用问题
+function deepClone1(target, map = new WeakMap()) {
+  // 如果目标是基本类型或者null，直接返回
+  if (typeof target !== "object" || target === null) {
+    return target;
+  }
+
+  // 检查这个对象是否已经被复制过
+  if (map.has(target)) {
+    return map.get(target);
+  }
+
+  let clone = Array.isArray(target) ? [] : {};
+  // 存储已经复制的对象
+  map.set(target, clone);
+
+  for (let key in target) {
+    if (Object.prototype.hasOwnProperty.call(target, key)) {
+      // 递归克隆
+      clone[key] = deepClone1(target[key], map);
+    }
   }
   return clone;
 }
@@ -34,4 +60,20 @@ let originalObj = {
   address: { city: "New York", country: "USA" },
 };
 let clonedObj = deepClone(originalObj);
-console.log(clonedObj); // { name: 'John', age: 30, address: { city: 'New York', country: 'USA' } }
+// console.log(clonedObj); // { name: 'John', age: 30, address: { city: 'New York', country: 'USA' } }
+
+let b = [1, 2, 3];
+// console.log(deepClone(b));
+
+const target = {
+  field1: 1,
+  field2: undefined,
+  field3: {
+    child: "child",
+  },
+
+  field4: [2, 4, 8],
+};
+target.target = target;
+
+console.log(deepClone1(target));
