7eec19494cf12fe852dd75ea8c212747299df26f
liuyilong <redtea25@qq.com>
2024-06-03 21:01:41 +0800

    原生函数：reverse

---
+++ 原生函数的实现/reverse.js
@@ -0,0 +1,43 @@
+// reverse 函数的实现
+
+/**
+ * reverse() 方法就地反转数组中的元素。
+ * @returns {Array} 返回一个新的数组
+ */
+Array.prototype.myReverse = function () {
+  let left = 0;
+  let right = this.length - 1;
+
+  while (left < right) {
+    [this[left], this[right]] = [this[right], this[left]];
+    left++;
+    right--;
+  }
+  return this;
+};
+
+// 测试
+let array = [1, 2, 3, 4, 5];
+console.log(array.myReverse()); // [5, 4, 3, 2, 1]
+
+// ---------------------------------------------------
+
+// toReverse 函数的实现
+/**
+ * 将数组中的元素顺序反转
+ * @param {Array} arr - 待反转的数组
+ * @returns {Array} 返回一个新的数组
+ */
+function toReverse(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  //   let res = arr.slice();
+  let res = [...arr];
+
+  while (left < right) {
+    [res[left], res[right]] = [res[right], res[left]];
+    left++;
+    right--;
+  }
+  return res;
+}
