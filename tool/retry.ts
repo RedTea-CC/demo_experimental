23df53e15eb359fe9337cc14d7178068dd89403a
liuyilong <redtea25@qq.com>
2025-02-08 16:45:49 +0800

    feat:错误重试函数

---
+++ tool/retry.ts
@@ -0,0 +1,26 @@
+// 实现一个重试方法，要求传入一个异步函数和n(重试次数)，当函数执行n次失败停止执行
+const retry = async (fn: () => Promise<any>, n: number) => {
+  let i = 0;
+  while (i < n) {
+    console.log(i);
+    try {
+      return await fn();
+    } catch (e) {
+      i++;
+    }
+  }
+};
+
+const mockFn = async () => {
+  console.log("try");
+  // return Promise.resolve("noraml");
+  return Promise.reject("error");
+};
+
+retry(mockFn, 3)
+  .then((res) => {
+    console.log("res", res);
+  })
+  .catch((e) => {
+    console.log("e", e);
+  });
