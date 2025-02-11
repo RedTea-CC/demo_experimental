6d8f4796ae582a89b37475e7f097dcf80cbe2be5
liuyilong <redtea25@qq.com>
2025-02-11 16:32:28 +0800

    feat:发布订阅模式

---
+++ DesignPattern/EventEmitter.js
@@ -0,0 +1,42 @@
+// 发布订阅模式
+class EventEmitter {
+  constructor() {
+    this.eventsMap = new Map();
+  }
+
+  // 订阅事件
+  on(eventName, callback) {
+    const newArry = this.eventsMap.get(eventName) || [];
+    newArry.push(callback);
+    this.eventsMap.set(eventName, newArry);
+  }
+
+  // 发布事件
+  emit(event, ...args) {
+    if (!this.eventsMap.has(event)) return;
+
+    this.eventsMap.get(event).forEach((fn) => fn(...args));
+  }
+
+  off(event) {
+    this.eventsMap.delete(event);
+  }
+
+  once(event, callback) {
+    const onceCallback = (...args) => {
+      callback(...args);
+      this.off(event);
+    };
+    this.on(event, onceCallback);
+  }
+}
+
+// 结合单例和非单例：通过工厂函数灵活创建实例，满足不同需求
+
+// 导出一个全局单例实例
+const globalEventEmitter = new EventEmitter();
+
+// 导出一个工厂函数，用于创建新的实例
+export const createEventEmitter = () => new EventEmitter();
+
+export default globalEventEmitter;
