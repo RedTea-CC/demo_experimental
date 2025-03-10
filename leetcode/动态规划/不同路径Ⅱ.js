26fdd5a820d76e1d90ecfbd1fd94148aa0c4810b
liuyilong <redtea25@qq.com>
2025-03-10 15:10:22 +0800

    不同路径Ⅱ优化

--- leetcode/动态规划/不同路径Ⅱ.js
+++ leetcode/动态规划/不同路径Ⅱ.js
@@ -4,32 +4,25 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function (obstacleGrid) {
+  if (obstacleGrid[0][0] === 1) return 0;
   const m = obstacleGrid.length,
     n = obstacleGrid[0].length;
 
   const f = new Array(m).fill(0).map(() => new Array(n).fill(0));
+  f[0][0] = 1;
 
-  for (let i = 0; i < m; i++) {
-    if (obstacleGrid[i][0] === 1) {
-      f[i][0] = 0;
-      break;
-    }
+  for (let i = 1; i < m; i++) {
+    if (obstacleGrid[i][0] === 1) break;
     f[i][0] = 1;
   }
-  for (let j = 0; j < n; j++) {
-    if (obstacleGrid[0][j] === 1) {
-      f[0][j] = 0;
-      break;
-    }
+  for (let j = 1; j < n; j++) {
+    if (obstacleGrid[0][j] === 1) break;
     f[0][j] = 1;
   }
 
   for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {
-      if (obstacleGrid[i][j] === 1) {
-        f[i][j] = 0;
-        continue;
-      }
+      if (obstacleGrid[i][j] === 1) continue;
       f[i][j] = f[i - 1][j] + f[i][j - 1];
     }
   }
