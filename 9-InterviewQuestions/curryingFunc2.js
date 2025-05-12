7ea1d76d80b4cfd5f7589323c635b0a37bd61d5e
liuyilong <redtea25@qq.com>
2025-05-12 11:24:50 +0800

    feat: 函数柯里化

---
+++ 9-InterviewQuestions/curryingFunc2.js
@@ -0,0 +1,14 @@
+// 函数柯里化
+
+// 柯里化是一种将使用多个参数的一个函数转换成一系列使用一个参数的函数的技服
+// 柯里化的主要作用是参数复用、提前返回和延迟执行
+function currying(fn, ...args) {
+  console.log("目标函数的参数个数:", fn.length, "当前已经传入的参数个数:", args.length);
+  return fn.length <= args.length ? fn(...args) : currying.bind(null, fn, ...args);
+}
+
+// 拆分函数和收集参数
+const sum = currying((a, b, c, d) => a + b + c + d);
+
+console.log(sum(1)(2)(3)(4)); // 10
+console.log(sum(1, 2, 3, 4)); // 10
