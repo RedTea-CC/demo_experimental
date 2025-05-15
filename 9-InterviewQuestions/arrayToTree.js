7d993c69eb51827e87a54749143c9362604d6477
liuyilong <redtea25@qq.com>
2025-05-15 14:57:53 +0800

    feat: 添加数组转树的实现和示例

--- 9-InterviewQuestions/arrayToTree.js
+++ 9-InterviewQuestions/arrayToTree.js
@@ -19,6 +19,26 @@ function arrayToTree(arr) {
   return roots;
 }
 
+function arrayToTree1(array) {
+  const idMap = {};
+  const result = [];
+  array.forEach((item) => {
+    idMap[item.id] = { ...item, children: [] };
+  });
+  array.forEach((item) => {
+    if (item.parent === null) {
+      result.push(idMap[item.id]);
+    } else {
+      idMap[item.parent].children.push(idMap[item.id]);
+    }
+  });
+  return result;
+}
+
+// `idMap` 中存储的是新对象的引用。后续你在 `idMap[parentId].children.push(...)` 时，实际上是修改了这个对象的 `children` 属性。
+// 而 `result.push(idMap[item.id])` 时，`result` 里存放的也是同一个对象的引用。
+// 所以，无论你通过 `idMap` 还是 `result` 访问这个对象，看到的都是同一个对象，修改它的属性会同步反映在所有引用上。
+
 let tree = arrayToTree([
   { id: 1, value: 1, parent: null },
   { id: 2, value: 2, parent: 1 },
--- demo.js
+++ demo.js
@@ -1,9 +1,27 @@
-// 函数柯里化
+// ------组合函数
 
-function currying(fn, ...args) {
-  return args.length >= fn.length ? fn(...args) : currying.bind(null, fn, ...args);
+function compose(...fns) {
+  return (...values) => {
+    return fns.reduceRight((acc, fn) => fn(acc), values[0]);
+  };
 }
 
-const sum = (a, b, c, d) => a + b + c + d;
+var add1 = (x) => Number(x) + 1;
+var mul5 = (x) => Number(x) * 5;
+console.log(compose(mul5, add1)(2)); // =>15
 
-console.log(currying(sum)(1)(2)(3)(4)); // 10
+// ------函数柯里化
+
+function curry(fn, ...args) {
+  if (fn.length <= args.length) {
+    return fn(...args);
+  }
+  return curry.bind(null, fn, ...args);
+}
+
+const sum = curry((a, b, c, d) => a + b + c + d);
+
+console.log(sum(1)(2)(3)(4)); // 10
+console.log(sum(1, 2, 3, 4)); // 10
+
+// ------end
