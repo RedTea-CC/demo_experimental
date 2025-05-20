4538a1cb0d7045767ef1afaa07d4b365a1eb5d9a
liuyilong <redtea25@qq.com>
2025-05-20 15:47:31 +0800

    feat:添加注释

--- demo copy.js
+++ demo copy.js
@@ -1,10 +1,22 @@
+globalThis.name = "golbal-name";
+
 let obj = {
   name: "123",
   sayHi: () => {
+    // 这里的 this 是 window
+    // 箭头函数中的 this 是定义时的 this
     console.log(`hi,${this?.name}`);
+  },
+  sayHi2: function () {
+    console.log(`2hi,${this?.name}`);
+  },
+  sayHi3() {
+    console.log(`3hi,${this?.name}`);
   },
 };
 
+// 箭头函数中的 this 是定义时的 this
+// 普通函数中的 this 是调用时的 this
 obj.sayHi();
-
-console.log(global);
+obj.sayHi2();
+obj.sayHi3();
